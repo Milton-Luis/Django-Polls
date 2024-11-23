e44bbb84a5848c5054672ea9d41ac85bd22e7db3
Milton-Luis <miltonluis138@gmail.com>
2024-11-22 22:05:58 -0300

    refactor: update was_published_recently function

--- project/apps/polls/models.py
+++ project/apps/polls/models.py
@@ -8,11 +8,13 @@ class Question(models.Model):
     question_text = models.CharField(max_length=20)
     pub_date = models.DateTimeField("Data de publicação")
 
+
     def __str__(self):
         return self.question_text
-    
+
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta()
+        now = timezone.now()
+        return now- datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
